2e28f6a4c5edc9808cab93602d909780e8009957
olifog <oliverfogelin@gmail.com>
2025-02-08 20:52:49 +0000

    big rocks

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,10 +1,10 @@
 extends Control
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://3DLevel.tscn")
+	get_tree().change_scene_to_file("res://scenes/3DLevel.tscn")
 
 func _on_settings_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://settings.tscn")
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
 	
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
